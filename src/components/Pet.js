0aca7cf808fbb265fcb90f876885244b1dc89116
Giovanni Rodriguez <giothedesigner@gmail.com>
2025-03-26 14:07:19 -0700

    finished project

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 export default function Pet(props) {
   const { pet } = props
+  const [fav, setFav] = useState(false);
 
   return (
     <div className='pet-friends container'>
       <p>{pet.name} ({pet.type})</p>
+      {fav ? "❤️" : <button onClick={() => setFav(true)}>Fave</button>}
     </div>
   )
 }
